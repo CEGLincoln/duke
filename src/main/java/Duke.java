8642c3d55f2bd43255cb1edaa1fade15963a77ad
CEGLincoln <e0311003@u.nus.edu>
2019-08-21 15:14:53 +0800

    level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
-public class Duke {
+import java.util.*;
+
+public class Duke{
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //DECLARE
+        String str = "This is the\n"
+            + " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n\n"
+            + "Hello! What can I do for you?";
+        Scanner scanner = new Scanner(System.in);
+        Boolean stay = true;
+        //SHOW
+        System.out.println(str);
+        //UH OH
+        while(stay){
+            str = scanner.nextLine();
+            switch(str){
+                case "bye":
+                    stay = false;
+                    break;
+                default:
+                    System.out.println(str);
+            }
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+        scanner.close();
     }
 }
