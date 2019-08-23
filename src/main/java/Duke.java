dffa4d5fca20a3e7a802275d0e0f625e14ca6648
CEGLincoln <e0311003@u.nus.edu>
2019-08-23 12:33:40 +0800

    level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,36 +1,53 @@
 import java.util.*;
 
 public class Duke{
+    public static void printStr(String str) {
+        System.out.println(str);
+    }
+
     public static void main(String[] args) {
         //DECLARE
         String str = "This is the\n"
-            + " ____        _        \n"
-            + "|  _ \\ _   _| | _____ \n"
-            + "| | | | | | | |/ / _ \\\n"
-            + "| |_| | |_| |   <  __/\n"
-            + "|____/ \\__,_|_|\\_\\___|\n\n"
-            + "Hello! What can I do for you?";
+                + " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n\n"
+                + "Hello! What can I do for you?";
         Scanner scanner = new Scanner(System.in);
         Boolean stay = true;
-        List<String> stuff = new ArrayList<String>();
-        //SHOW
-        System.out.println(str);
-        //UH OH
+        ArrayList<Task> stuff = new ArrayList<Task>();
+        int x = 0;
+
+        //START HERE
+        printStr(str);
         while(stay){
             str = scanner.nextLine();
-            switch(str){
+            String[] part = str.split(" ", 2);
+            switch(part[0]){
                 case "bye":
                     stay = false;
                     break;
+                case "done":
+                    x = Integer.parseInt(part[1]) - 1;
+                    stuff.get(x).markAsDone();
+                    printStr("Nice! I've marked this task as done:");
+                    printStr("[" + stuff.get(x).getStatusIcon() + "] " + stuff.get(x).getDescription());
+                    break;
                 case "list":
-                    System.out.println(stuff);
+                    printStr("Here are the tasks in your list:");
+                    x = 0;
+                    for(Task t : stuff){
+                        x++;
+                        printStr(x + ".[" + t.getStatusIcon() + "] " + t.getDescription());
+                    }
                     break;
                 default:
-                    stuff.add(str);
-                    System.out.println("added: " + str);
+                    stuff.add(new Task(str));
+                    printStr("added: " + str);
             }
         }
-        System.out.println("Bye. Hope to see you again soon!");
+        printStr("Bye. Hope to see you again soon!");
         scanner.close();
     }
 }
